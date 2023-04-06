053238736f00ed4271fb52a1b814e52c858d9a80
Viet Tai <viettai@MBVietTai.local>
2023-04-06 21:04:36 +0700

    dsa

--- index.js
+++ index.js
@@ -4,34 +4,21 @@ const app = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-    // optionSuccessStatus: 200,
-  })
-);
-// app.use(function (req, res, next) {
-//   // Website you wish to allow to connect
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   // Request methods you wish to allow
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//   );
-
-//   // Request headers you wish to allow
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, Content-Type, Accept, Authorization, X-Request-With"
-//   );
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-//   // Pass to next layer of middleware
-//   // res.setHeader('Content-Type', 'application/json; charset=utf-8');
-//   next();
-// });
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     credentials: true,
+//     // optionSuccessStatus: 200,
+//   })
+// );
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
