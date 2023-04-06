0ebabd018a276c570d69808cf8ea798815f31f4e
Viet Tai <viettai@MBVietTai.local>
2023-04-06 20:33:28 +0700

    dsa

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     // Request methods you wish to allow
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     
     // Request headers you wish to allow
-    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Request-With');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Request-With');
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    res.header('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Credentials', true);
     // Pass to next layer of middleware
     // res.setHeader('Content-Type', 'application/json; charset=utf-8');
     next();
