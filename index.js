de3248b93400e38a6723acf6670f9f53c2233095
Viet Tai <viettai@MBVietTai.local>
2023-04-06 21:09:22 +0700

    dsa

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const cors = require("cors");
 //   })
 // );
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Max-Age", "1800");
   res.setHeader("Access-Control-Allow-Headers", "content-type");
