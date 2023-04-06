437521764dbe1b1a2c83060915d24a0a503ddd7d
Viet Tai <viettai@MBVietTai.local>
2023-04-06 20:09:02 +0700

    done

--- controllers/like.js
+++ controllers/like.js
@@ -1,16 +1,20 @@
 const db = require("../db");
 const moment = require('moment');
 const addLike = async (req, res) => {
-  const q = "INSERT INTO likes(`userId`,`postId`) VALUES(?);";
+  const q = "INSERT INTO likes(`userId`,`postId`,`createdAt`) VALUES(?);";
   const date = moment().locale('vi').format('YYYY-MM-DD HH:mm:ss');
-  db.query(q, [[req.userInfo.id, req.body.postId , date]], (err, data) => {
-    if (err)
-      return res
-        .status(404)
-        .json({ message: "Error when getting likes", error: err });
-    if (data.affectedRows <= 0)
-      return res.status(404).json({ message: "Update failed" });
-  });
+  db.query(
+    q,
+    [[req.body.senderId, req.body.postId , date]],
+    (err, data) => {
+      if (err)
+        return res
+          .status(404)
+          .json({ message: "Error when add like", error: err });
+      if (data.affectedRows <= 0)
+        return res.status(404).json({ message: "Update failed" });
+    }
+  );
   const e =
     "SELECT * FROM likes WHERE userId = ? AND postId = ? ORDER BY id DESC LIMIT 1 ;";
   db.query(e, [req.userInfo.id, req.body.postId], (err, data) => {
@@ -19,7 +23,7 @@ const addLike = async (req, res) => {
 };
 const getLikes = (req, res) => {
   const q =
-    "SELECT DISTINCT l.*  FROM likes AS l JOIN users AS u ON (l.userId = u.id) JOIN posts AS p ON (l.postId = p.id) ;";
+    "SELECT DISTINCT l.userId , l.postId   FROM likes AS l JOIN users AS u ON (l.userId = u.id) JOIN posts AS p ON (l.postId = p.id) ;";
   db.query(q, (err, data) => {
     if (err)
       return res
@@ -30,7 +34,7 @@ const getLikes = (req, res) => {
 };
 const deleteLike = (req, res) => {
   const q = "DELETE FROM likes WHERE userId = ? AND postId = ?;";
-  db.query(q, [req.userInfo.id, req.params.postId ], (err, data) => {
+  db.query(q, [req.userInfo.id, req.params.postId], (err, data) => {
     if (err)
       return res
         .status(404)
