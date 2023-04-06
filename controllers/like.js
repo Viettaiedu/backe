658a2d001ea1c1b544eec0da99cb6d297396f664
Viet Tai <viettai@MBVietTai.local>
2023-04-06 18:49:55 +0700

    handle notifi like

--- controllers/like.js
+++ controllers/like.js
@@ -1,22 +1,25 @@
 const db = require("../db");
+const moment = require('moment');
 const addLike = async (req, res) => {
   const q = "INSERT INTO likes(`userId`,`postId`) VALUES(?);";
-  db.query(q, [[req.userInfo.id, req.body.postId]], (err, data) => {
+  const date = moment().locale('vi').format('YYYY-MM-DD HH:mm:ss');
+  db.query(q, [[req.userInfo.id, req.body.postId , date]], (err, data) => {
     if (err)
       return res
         .status(404)
         .json({ message: "Error when getting likes", error: err });
-        if(data.affectedRows <= 0) return res.status(404).json({message:"Update failed"})
-        
+    if (data.affectedRows <= 0)
+      return res.status(404).json({ message: "Update failed" });
   });
-  const e = "SELECT * FROM likes WHERE userId = ? AND postId = ? ORDER BY id DESC LIMIT 1 ;";
-        db.query(e ,[req.userInfo.id, req.body.postId], (err,data) => {
-          res.status(201).json(data[0]);
-        })
+  const e =
+    "SELECT * FROM likes WHERE userId = ? AND postId = ? ORDER BY id DESC LIMIT 1 ;";
+  db.query(e, [req.userInfo.id, req.body.postId], (err, data) => {
+    res.status(201).json(data[0]);
+  });
 };
 const getLikes = (req, res) => {
   const q =
-    "SELECT DISTINCT l.*  FROM likes AS l JOIN users AS u ON (l.userId = u.id) JOIN posts AS p ON (l.postId = p.id);";
+    "SELECT DISTINCT l.*  FROM likes AS l JOIN users AS u ON (l.userId = u.id) JOIN posts AS p ON (l.postId = p.id) ;";
   db.query(q, (err, data) => {
     if (err)
       return res
@@ -26,9 +29,8 @@ const getLikes = (req, res) => {
   });
 };
 const deleteLike = (req, res) => {
-  const q =
-    "DELETE FROM likes WHERE userId = ? AND postId = ?;";
-  db.query(q,[req.userInfo.id,req.params.postId], (err, data) => {
+  const q = "DELETE FROM likes WHERE userId = ? AND postId = ?;";
+  db.query(q, [req.userInfo.id, req.params.postId ], (err, data) => {
     if (err)
       return res
         .status(404)
