53c7b1443f50539f363c2f5495f27222bbb3eaec
Viet Tai <viettai@MBVietTai.local>
2023-04-06 22:04:57 +0700

    dsada

--- index.js
+++ index.js
@@ -20,26 +20,26 @@ const routesMessenges = require("./routes/messenges");
 const routesNotifications = require("./routes/notifications");
 
 
-// app.use(function (req, res, next) {
-//   // Website you wish to allow to connect
-//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-//   // Request methods you wish to allow
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//   );
-//   // Request headers you wish to allow
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, Content-Type, Accept, Authorization, X-Request-With"
-//   );
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-//   // Pass to next layer of middleware
-//   res.setHeader("Content-Type", "application/json; charset=utf-8");
-//   next();
-// });
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, Content-Type, Accept, Authorization, X-Request-With"
+  );
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  // Pass to next layer of middleware
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+  next();
+});
 app.use(cors({
   origin:'http://localhost:3000',
   credentials :true
