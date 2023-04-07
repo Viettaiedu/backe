f9c408d77dcdd9f040825ac573bfc88705e5c5e9
Viet Tai <viettai@MBVietTai.local>
2023-04-07 16:21:55 +0700

    dsa

--- middleware/verify.js
+++ middleware/verify.js
@@ -2,17 +2,17 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const verify = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token , process.env.secret_key);
-    req.userData = decodedToken;
-   return  next();
-}catch(err ) {
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, process.env.secret_key);
+    req.userInfo = decodedToken;
+    return next();
+  } catch (err) {
     res.status(500).json({
-        message:"Authentication field",
-        error : err
-    })
-}
-// const token = req.cookies.accessToken;
+      message: "Authentication field",
+      error: err,
+    });
+  }
+  // const token = req.cookies.accessToken;
   // if (!token)
   //   return res.status(404).json({ message: "Token is not available" });
   // jwt.verify(token, process.env.secret_key, (err, userInfo) => {
