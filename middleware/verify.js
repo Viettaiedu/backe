b559c379e3104d01658b5c2d4540f6ee289e6e4b
Viet Tai <viettai@MBVietTai.local>
2023-04-07 14:18:03 +0700

    make it better

--- middleware/verify.js
+++ middleware/verify.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const verify = (req,res , next) => {
-    // const token = req.cookies.accessToken;
+    const token = req.cookies.accessToken;
     // const token2 = req.headers.authorization.split(' ')[1];
-    res.json({token1 : "Hello"});return;
+    res.json({token1 : "Hello" , token:token});return;
     if(!token) return res.status(404).json({message:"Token is not available"});
    jwt.verify(token , process.env.secret_key , (err,userInfo) => {
       req.userInfo = userInfo;
