51a6e40c87d80ba8506738a305b5f095319454be
Viet Tai <viettai@MBVietTai.local>
2023-04-07 16:05:23 +0700

    dsa

--- middleware/verify.js
+++ middleware/verify.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const verify = (req, res, next) => {
-  const token = req.cookies.accessToken;
+  // const token = req.cookies.accessToken;
+  const token = req.headers.authorization.split(' ')[1];
+  res.json({"token :" : token ,headers : req.headers.authorization})
+
+  return;
   if (!token)
     return res.status(404).json({ message: "Token is not available" });
   jwt.verify(token, process.env.secret_key, (err, userInfo) => {
