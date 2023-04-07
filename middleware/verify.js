625786d61a0ae6fcc8547452a2209bf15d40bbf1
Viet Tai <viettai@MBVietTai.local>
2023-04-07 14:42:45 +0700

    make it better

--- middleware/verify.js
+++ middleware/verify.js
@@ -1,15 +1,19 @@
-require('dotenv').config();
-const jwt = require('jsonwebtoken');
-const verify = (req,res , next) => {
-    const token = req.cookies.accessToken;
-    // const token2 = req.headers.authorization.split(' ')[1];
-    res.json({token1 : "Hello" , token:token + "My1123"});return;
-    if(!token) return res.status(404).json({message:"Token is not available"});
-   jwt.verify(token , process.env.secret_key , (err,userInfo) => {
-      req.userInfo = userInfo;
-      next();
-   })
-}
+require("dotenv").config();
+import Cookies from "js-cookie";
+const jwt = require("jsonwebtoken");
+const verify = (req, res, next) => {
+  const token = req.cookies.accessToken;
+
+  // const token2 = req.headers.authorization.split(' ')[1];
+  res.json({ token1: "Hello", token: Cookies.get("accessToken") + "My1123" });
+  return;
+  if (!token)
+    return res.status(404).json({ message: "Token is not available" });
+  jwt.verify(token, process.env.secret_key, (err, userInfo) => {
+    req.userInfo = userInfo;
+    next();
+  });
+};
 module.exports = {
-    verify
-}
+  verify,
+};
